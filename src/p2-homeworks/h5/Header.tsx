ba1cdfbfa5429d38b98f62c1039477a42070d89c
Victor Adamovich <ndptrv@yandex.by>
2022-05-17 12:00:28 +0300

    Add hw5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom';
+import c from './Header.module.css';
 
 function Header() {
     return (
-        <div>
-            <NavLink to='/pre-junior'>pre-junior</NavLink>--
-            <NavLink to='/junior'>junior</NavLink>--
-            <NavLink to='/junior+'>junior+</NavLink>
+        <div className={c.HeaderNavi}>
+            <NavLink className={c.HeaderNavi_item} to='/pre-junior'>pre-junior</NavLink>
+            <NavLink className={c.HeaderNavi_item} to='/junior'>junior</NavLink>
+            <NavLink className={c.HeaderNavi_item} to='/junior+'>junior+</NavLink>
+            <span className={c.box}> </span>
         </div>
     )
 }
